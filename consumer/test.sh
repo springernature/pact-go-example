c6e03ad6f703d7641a9e0a639db01fd291cf4efb
Holger Protzek <holger.protzek@springer.com>
2018-04-16 13:51:31 +0200

    run daemon in background

--- consumer/test.sh
+++ consumer/test.sh
@@ -2,6 +2,6 @@
 
 wget https://github.com/pact-foundation/pact-go/releases/download/v0.0.12/pact-go_linux_amd64.tar.gz
 tar xvzf pact-go_linux_amd64.tar.gz
-./pact-go daemon &&
+./pact-go daemon &
 
 go test
