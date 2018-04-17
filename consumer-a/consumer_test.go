8a286ced568712dcc7b35419004715062cf96983
Holger Protzek <holger.protzek@springer.com>
2018-04-17 16:05:24 +0200

    new new new

--- consumer-a/consumer_test.go
+++ consumer-a/consumer_test.go
@@ -73,7 +73,7 @@ func TestUpload(t *testing.T) {
 	err := p.Publish(types.PublishRequest{
 		PactURLs:        []string{filepath.FromSlash(fmt.Sprintf("%s/example-consumer-a-example-provider.json", pactDir))},
 		PactBroker:      "https://pact.halfpipe.io",
-		ConsumerVersion: "3.0.4",
+		ConsumerVersion: "3.0.5",
 		Tags:            []string{"latest", "stable"},
 		BrokerUsername:  os.Getenv("PACT_BROKER_USERNAME"),
 		BrokerPassword:  os.Getenv("PACT_BROKER_PASSWORD"),
